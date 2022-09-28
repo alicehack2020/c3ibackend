40ccb359aba204752cf3148b79a8e0b8f455097a
Mangesh2042 <mangesh2042@gmail.com>
2022-09-28 12:24:55 +0530

    error

--- model/Video.js
+++ model/Video.js
@@ -6,11 +6,11 @@ const videoSchema=new mongoose.Schema({
     course_id:{type:String,required:true,trim:true},
     title:{type:String,required:true,trim:true},
     info:{type:String,required:true,trim:true},
-    image_url:{type:String,required:true,trim:true},
-    video_url:{type:String,required:true,trim:true},
-    pdf_url:{type:String,required:true,trim:true},
-    text_url:{type:String,required:true,trim:true},
-    doc_url:{type:String,required:true,trim:true}
+    image_url:{type:String,trim:true},
+    video_url:{type:String,trim:true},
+    pdf_url:{type:String,trim:true},
+    text_url:{type:String,trim:true},
+    doc_url:{type:String,trim:true}
 })
 
 //Model
